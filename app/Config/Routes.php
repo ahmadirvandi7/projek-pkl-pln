73ca296ad028c122c14a7ee3cf5b5487b1fc9b63
AhmadIrvandi <ahmadirvandi637@gmail.com>
2024-01-03 11:23:59 +0700

    desain tampilan

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,13 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+
+$routes->get('/', 'Home::login');
+
+$routes->get('/register', 'Home::register');
+
+$routes->get('/beranda_admin', 'Home::beranda_admin');
+$routes->get('/jadwalrapat_admin', 'Home::jadwalrapat_admin');
+$routes->get('/datadriver_admin', 'Home::datadriver_admin');
+$routes->get('/datatamu_admin', 'Home::datatamu_admin');
+
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,35 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+    public function login(): string
+    {
+        return view('login');
+    }
+
+    public function register(): string
+    {
+        return view('register');
+    }
+
+    public function beranda_admin(): string
+    {
+        return view('beranda_admin');
+    }
+
+    public function jadwalrapat_admin(): string
+    {
+        return view('jadwalrapat_admin');
+    }
+
+    public function datadriver_admin(): string
+    {
+        return view('datadriver_admin');
+    }
+
+    public function datatamu_admin(): string
+    {
+        return view('datatamu_admin');
+    }
+
 }
