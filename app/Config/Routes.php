ca1f37c29af32265ffef04929476ea1e488b8207
AhmadIrvandi <ahmadirvandi637@gmail.com>
2024-01-08 08:53:23 +0700

    tambahan

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,8 +10,26 @@ $routes->get('/', 'Home::login');
 
 $routes->get('/register', 'Home::register');
 
+
+//admin
 $routes->get('/beranda_admin', 'Home::beranda_admin');
 $routes->get('/jadwalrapat_admin', 'Home::jadwalrapat_admin');
 $routes->get('/datadriver_admin', 'Home::datadriver_admin');
 $routes->get('/datatamu_admin', 'Home::datatamu_admin');
+
+
+//superadmin
+$routes->get('/beranda_superadmin', 'Home::beranda_superadmin');
+$routes->get('/jadwalrapat_superadmin', 'Home::jadwalrapat_superadmin');
+$routes->get('/datadriver_superadmin', 'Home::datadriver_superadmin');
+$routes->get('/datatamu_superadmin', 'Home::datatamu_superadmin');
+
+
+//admin humas
+$routes->get('/beranda_adminhumas', 'Home::beranda_adminhumas');
+$routes->get('/jadwalrapat_adminhumas', 'Home::jadwalrapat_adminhumas');
+$routes->get('/datadriver_adminhumas', 'Home::datadriver_adminhumas');
+$routes->get('/datatamu_adminhumas', 'Home::datatamu_adminhumas');
+
+
 
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -19,6 +19,7 @@ class Home extends BaseController
         return view('register');
     }
 
+    //admin
     public function beranda_admin(): string
     {
         return view('beranda_admin');
@@ -37,6 +38,48 @@ class Home extends BaseController
     public function datatamu_admin(): string
     {
         return view('datatamu_admin');
+    }
+
+    //superadmin
+    public function beranda_superadmin(): string
+    {
+        return view('beranda_superadmin');
+    }
+
+    public function jadwalrapat_superadmin(): string
+    {
+        return view('jadwalrapat_superadmin');
+    }
+
+    public function datadriver_superadmin(): string
+    {
+        return view('datadriver_superadmin');
+    }
+
+    public function datatamu_superadmin(): string
+    {
+        return view('datatamu_superadmin');
+    }
+
+    //admin humas
+    public function beranda_adminhumas(): string
+    {
+        return view('beranda_adminhumas');
+    }
+
+    public function jadwalrapat_adminhumas(): string
+    {
+        return view('jadwalrapat_adminhumas');
+    }
+
+    public function datadriver_adminhumas(): string
+    {
+        return view('datadriver_adminhumas');
+    }
+
+    public function datatamu_adminhumas(): string
+    {
+        return view('datatamu_adminhumas');
     }
 
 }
